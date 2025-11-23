4a42ce7daae6c9825b0e72f204a8e8f4176536cf
Jefferson Archibald <archibaldjefferson17@gmail.com>
2025-11-23 22:08:01 +0000

    up

---
+++ alx-project-0x03/components/layouts/Header.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Button from "../common/button";
+
+const Header: React.FC = () => {
+    return (
+    <header className="fixed w-full bg-white shadow-md">
+        <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
+        <Link href="/" className="text-3xl md:text-5xl font-bold text-gray-800 tracking-tight"> Splash App
+        </Link>
+        {/* Button Group */}
+        <div className="flex gap-4">
+            <Button
+                        buttonLabel="Sign In"
+                        buttonBackgroundColor="red" buttonSize={""}/>
+            <Button
+                        buttonLabel="Sign Up"
+                        buttonBackgroundColor="blue" buttonSize={""} />
+        </div>
+        </div>
+    </header>
+    );
+};
+
+export default Header;
